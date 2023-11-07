c3c5abe3ef1a420c8f69c9c75c7a760252e9cc92
natabernhard <nata.bernhard@gmail.com>
2023-11-07 17:20:25 -0600

    JavaSCript Code Challenge Nov 2

---
+++ Code Challenge 5 November 2/index.js
@@ -0,0 +1,4 @@
+document.querySelector ("button").addEventListener ("click", handClick);
+function handClick () {
+    alert ("Good Bye!");
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+let x = 9;
+let y = 7;
+let z = x + y;
+console.log(z)
